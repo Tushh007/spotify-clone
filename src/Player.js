6c4a6172f732b8f30577a44cbce37cc3fe906cb2
Tushar Langer <tushar.langer@gmail.com>
2021-02-28 19:25:20 +0530

    spotify playbar developed

--- src/Player.js
+++ src/Player.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Body from './Body';
 import './Player.css';
+import Footer from './Footer';
 
 function Player({ spotify }) {
   return (
@@ -10,6 +11,7 @@ function Player({ spotify }) {
         <Sidebar />
         <Body />
       </div>
+      <Footer />
     </div>
   );
 }
