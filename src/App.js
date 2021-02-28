0b8829d84661223f1f6e844219eec64e74f3e9b1
Tushar Langer <tushar.langer@gmail.com>
2021-02-28 14:30:17 +0530

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
+import { useDataLayerValue } from './DataLayer';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Login from './Login';
+import Player from './Player';
 import './App.css';
 
+const spotify = new SpotifyWebApi();
+
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+    }
+  }, []);
+
+  console.log('user', user);
+  console.log('token', token);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+  user: null,
+  token:
+    'BQDSdEG2buxSzZne6R8f25Gco51HnKe9qHu4P3t2xD2931xVeGe7ttlt42kK-NyuLEtzkNFHwbZ3-HKV7XPIZqIqEaCN5mHYtJtqQq8S8HvCjioIV2-f-XsVOsUtRMEfplp0hWqkVIhWG_cn0Y0Ob_ZAfuzHwA2pQfOJDZJBwRbdiTKXYefi',
+  playlists: [],
+  playing: false,
+  item: null,
+};
+
+const reducer = (state, action) => {
+  // action -> type, [payload]
+  console.log(action);
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
